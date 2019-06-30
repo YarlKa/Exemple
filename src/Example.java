6a5b81305f98d88d7dd1426e09d8a9310bb88c7e
Yaroslav <Kaminski.y@mail.ru>
2019-06-30 11:13:56 +0300

    Различие с print и println

--- src/Example.java
+++ src/Example.java
@@ -1,6 +1,9 @@
 public class Example {
     public static void main(String[] args) {
         System.out.println("Java правит Интернетом");
+        System.out.print("Но ");
+        System.out.print("это ");
+        System.out.print("не точно! ");
 
     }
 }
